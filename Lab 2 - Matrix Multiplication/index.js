8078466630be2d1905f4a61abc3930c3d3e52ae5
Konstantin Belousov <kombuchamp@gmail.com>
2020-03-01 21:28:34 +0300

    Add charts view

---
+++ Lab 2 - Matrix Multiplication/index.js
@@ -0,0 +1,9 @@
+const standardEven = document.getElementById('standardEven');
+const standardUneven = document.getElementById('standardUneven');
+const winogradEven = document.getElementById('winogradEven');
+const winogradUneven = document.getElementById('winogradUneven');
+
+const standardEvenChart = new Chart(standardEven);
+const standardUnevenChart = new Chart(standardUneven);
+const winogradEvenChart = new Chart(winogradEven);
+const winogradUnevenChart = new Chart(winogradUneven);
