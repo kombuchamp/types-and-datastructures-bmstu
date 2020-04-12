65ad9604fe10e12cb308577c9019a6e5b3e55411
Konstantin Belousov <kombuchamp@gmail.com>
2020-04-12 16:45:35 +0300

    Add lab-6

---
+++ Lab 6 - Reverse Polish Notation/src/app.js
@@ -0,0 +1,29 @@
+const Chart = require('chart.js');
+
+let ctx = document.getElementById('myChart').getContext('2d');
+
+let chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+        ],
+        datasets: [
+            {
+                label: 'My First dataset',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [0, 10, 5, 2, 20, 30, 45],
+            },
+        ],
+    },
+
+    // Configuration options go here
+    options: {},
+});
