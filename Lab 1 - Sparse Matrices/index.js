3b4b5305902fcd29672362081cc043d437067467
Konstantin Belousov <kombuchamp@gmail.com>
2020-05-24 19:59:16 +0300

    Implement matrix consuming

--- Lab 1 - Sparse Matrices/index.js
+++ Lab 1 - Sparse Matrices/index.js
@@ -1,48 +1,90 @@
-class SparseMatrix {
+/**
+ * Sparce matrix in compressed row format
+ */
+class CRS {
     constructor(matrix) {
-        this.matrix = {
-            AN: [],
-            NR: [],
-            NC: [],
-            JR: [],
-            JC: [],
-        };
+        /**
+         * Non-zero values
+         * @type {Array<number>}
+         */
+        this.values;
+        /**
+         * Column of the corresponding value
+         * @type {Array<number>}
+         */
+        this.columnIndex;
+        /**
+         * Index of the first value (of this.values) in the row
+         * @type {Array<number>}
+         */
+        this.rowIndex;
+
         if (matrix) {
-            this.pack(matrix);
+            this.consumeMatrix(matrix);
         }
     }
 
-    pack(matrix) {
-        const { AN, NR, NC, JR, JC } = this.matrix;
-        const rows = matrix.length;
-        const cols = matrix[0].length;
-        matrix.forEach((row, i) => {
-            let jr = undefined;
-            let nr = [];
-            row.forEach((item, j) => {
-                if (item === 0) return;
-                const idx = i * cols + j;
-                AN.push(item);
-                nr.push(AN.length - 1);
-                if (jr == null) jr = AN.length - 1;
-            });
-            JR.push(jr == null ? JR[JR.length - 1] | 0 : jr);
-            if (nr.length) nr.push(nr.shift());
-            NR.push(...nr);
-        });
-    }
+    /**
+     * Transforms matrix into compressed row format and saves it in this instance
+     * @param {number[][]} mat
+     */
+    consumeMatrix(mat) {
+        this.values = [];
+        this.columnIndex = [];
+        this.rowIndex = [];
 
-    toString() {
-        return Object.entries(this.matrix)
-            .map(([key, arr]) => `${key}: [${arr}]`)
-            .join('\n\r');
+        let currentFirstValue = -1;
+        let currentRow = 0;
+        mat.forEach((row, i) => {
+            row.forEach((val, j) => {
+                // assume we have integers
+                if (val !== 0) {
+                    this.values.push(val);
+                    this.columnIndex.push(j);
+                    if (currentFirstValue < 0) {
+                        currentFirstValue = this.values.length - 1;
+                    }
+                }
+            });
+            if (~currentFirstValue) {
+                while (currentRow <= i) {
+                    this.rowIndex.push(currentFirstValue);
+                    ++currentRow;
+                }
+            }
+            currentFirstValue = -1;
+        });
+        this.rowIndex.push(this.values.length);
     }
 }
 
-const mat = new SparseMatrix([
-    [1, 0, 2],
-    [0, 0, 0],
-    [0, 4, 2],
-]);
+console.log(
+    '----\n',
+    new CRS([
+        [0, 0, 0, 0],
+        [5, 8, 0, 0],
+        [0, 0, 3, 0],
+        [0, 6, 0, 0],
+    ])
+);
 
-console.log(mat.toString());
+console.log(
+    '----\n',
+    new CRS([
+        [0, 0, 1, 3, 0, 0, 0, 5, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 7, 0, 1, 0, 0],
+    ])
+);
+
+console.log(
+    '----\n',
+    new CRS([
+        [1, 0, 0, 0, 2, 0],
+        [0, 0, 3, 4, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 8, 0, 5],
+        [0, 0, 0, 0, 0, 0],
+        [0, 7, 1, 0, 0, 6],
+    ])
+);
